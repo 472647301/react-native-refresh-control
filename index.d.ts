99b20e59500fbede9151bd3685aa551de758c491
秃尾巴的猫 <472647301@qq.com>
2022-06-30 20:16:04 +0800

    Revert "Update index.d.ts"
    This reverts commit 4b5aa7399c83c8ecfcabba4cf3229e730f4728a0.

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,17 @@
 declare module "@byron-react-native/refresh-control" {
-  import { View } from "react-native";
+  import { ViewProps } from "react-native";
 
-  export class RNRefreshHeader extends View {}
-
-  export class RNRefreshControl extends View {
+  export interface RefreshControlProps extends ViewProps {
     refreshing?: boolean;
+    onRefresh?: () => Promise<void>;
     onChangeState?: (state: number) => void;
   }
-
-  export class RefreshControl extends View {
-    refreshing?: boolean;
+  export class RefreshControl extends React.Component<RefreshControlProps> {
     startRefresh: () => void;
     stopRefresh: () => void;
   }
+
+  export type ByronRefreshControlProps = Omit<RefreshControlProps, "onRefresh">;
+
+  export class ByronRefreshControl extends React.Component<ByronRefreshControlProps> {}
 }
