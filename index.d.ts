e5387e38452e53e188a3ab13b6b48ca06ded852d
秃尾巴的猫 <472647301@qq.com>
2022-06-30 20:20:57 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,20 @@
 declare module "@byron-react-native/refresh-control" {
   import { ViewProps } from "react-native";
 
+  export class RNRefreshHeader extends React.Component<ViewProps> {}
+
   export interface RefreshControlProps extends ViewProps {
     refreshing?: boolean;
     onRefresh?: () => Promise<void>;
     onChangeState?: (state: number) => void;
   }
+
+  export type RNRefreshControlProps = Omit<RefreshControlProps, "onRefresh">;
+
+  export class RNRefreshControl extends React.Component<RNRefreshControlProps> {}
+
   export class RefreshControl extends React.Component<RefreshControlProps> {
     startRefresh: () => void;
     stopRefresh: () => void;
   }
-
-  export type ByronRefreshControlProps = Omit<RefreshControlProps, "onRefresh">;
-
-  export class ByronRefreshControl extends React.Component<ByronRefreshControlProps> {}
 }
